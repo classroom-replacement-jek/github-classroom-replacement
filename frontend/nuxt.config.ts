d5ec20470fc2e3a806fcb906faa4e71b95e476c4
Edward-Mercado <edwardmercado149@gmail.com>
2026-09-15 11:11:18 -0400

    creation of project

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
+  css: ["~/assets/main.css"],
+  pages: true,
+  app: {
+    head: {
+      title: "Classroom",
+    },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    optimizeDeps: {
+      include: ["@vue/devtools-core", "@vue/devtools-kit", "reicon-vue"],
+    },
+  }
+})
